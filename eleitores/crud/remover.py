a2a25803d1b6247050479587e58d167084bae1ce
brunoterrapucc <bruno.ts4@puccampinas.edu.br>
2026-04-28 19:40:17 -0300

    adiciona a função remover eleitor

--- eleitores/crud/remover.py
+++ eleitores/crud/remover.py
@@ -1,3 +1,5 @@
+#feito por Bruno Terra
+#feito por Carlos Liberato
 from database.conexao import conectar
 import mysql.connector
 import eleitores.validacoes as validacoes
@@ -7,6 +9,50 @@ import eleitores.chaveAcesso as chaveAcesso
 import eleitores
 
 def removerEleitor():
-    pass
+    #remove um eleitor do bando de dados a partir do cpf informado
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+  #o cpf é validado usando um módulo existente
+    cpf = input("Insira o CPF do eleitor que deseja remover: ")
+
+    if not validacoes.validaCPF(cpf):
+        print("O CPF informado é inválido")
+
+        menus.menuGerenciamento()
+        return
+    
+    #criptografa o cpf antes de ser usado no banco
+    cpf_cripto = criptoCPF.criptoCPF(cpf)
+
+    #agora ocorre a verificação se existe
+    cursor.execute("SELECT  * FROM eleitores WHERE cpf = %s", cpf(cpf_cripto,))
+    eleitor = cursor.fetchone()
+
+    if eleitor is None:
+
+        print("Eleitor não encontrado")
+        menus.menuGerenciamento()
+        return
+    
+    #confirmação da remoção do eleitor
+    confirmacao = input("Tem certeza que deseja remover esse eleitor: (s/n)")
+
+    if confirmacao.lower != 's':
+        print("Confirmação cancelada")
+        menus.menuGerenciamento()
+        return
+    
+    #remove o eleitor
+
+    cursor.execute("DELETE FROM eleitores WHERE CPF = %s", cpf_cripto)
+    conexao.commit()
+
+
+    print("Eleitor foi removido com sucesso")
+
+    cursor.close()
+
+    conexao.close()
 
     menus.menuGerenciamento()
