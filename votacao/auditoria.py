2dabd74078e57521d12e3d9edafe4f8a9f286f92
nicolasreis-puccamp <nicolas.mgr@puccampinas.edu.br>
2026-05-07 20:43:47 -0300

    Reajustes no modulo auditoria.py

---
+++ votacao/auditoria.py
@@ -0,0 +1,17 @@
+# Desenvolvido por Nicolas Guimarães
+
+from database.conexao import conectar
+from datetime import datetime
+
+
+def registrarLog(mensagem):
+    #Registrar a mensagem no arquivo de log com timestamp no formato [YYYY-MM-DD HH:MM:SS]
+    pass
+
+def exibirLogs():
+    #Verificar se o arquivo de log existe e exibir seu conteúdo no terminal
+    pass
+
+def exibirProtocolos():
+    #Buscar todos os protocolos da tabela votos e exibir em ordem alfabética
+    pass
